bdb9a47ecb1e344e2e3ec08f34532ac33f453326
marcocastignoli <marco.castignoli@gmail.com>
2019-02-28 19:21:01 +0100

    Closes #4 Implement user authentication and authenticated API

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,83 @@
+var bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+var config = require('../instance.config.json')
 module.exports = ({ instance, dispatcher }) => {
+  /**
+   * Handle CORS requests
+  */
+  dispatcher.beforeFilter(/\//, function (req, res, chain) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    if (req.method === 'OPTIONS') {
+      res.setHeader('Access-Control-Allow-Headers', 'authentication')
+      res.end()
+    } else {
+      chain.next(req, res, chain)
+    }
+  })
+  /**
+   * The following middleware is attach on every request except /login
+   * Check if in the authentication header contains a jwt token:
+   *      - if the token is valid then continue the chain
+   *      - if the token is invalid reply with 401
+  */
+  dispatcher.beforeFilter(/\/(?!login)/, function (req, res, chain) {
+    try {
+      var decoded = jwt.verify(req.headers.authentication, config.secret)
+      if (decoded) {
+        chain.next(req, res, chain)
+      } else {
+        res.writeHeader(401)
+        res.end()
+      }
+    } catch (e) {
+      res.writeHeader(401)
+      res.end()
+    }
+  })
+  /**
+   * LOGIN
+   *
+   * @api {post} /login Login user
+   * @apiParam (Request body) {String} username Username
+   * @apiParam (Request body) {String} password Password
+   * @apiGroup Tasks
+   * @apiVersion 0.0.1
+   * @apiName LoginUser
+   * @apiGroup session
+   * @apiPermission none
+   * @apiSuccess {Bool} successful login
+   * @apiSuccessExample {json} Success
+   *    HTTP/1.1 200 OK
+   *    [{
+   *      "success": true,
+   *    }]
+   * @apiErrorExample {json} List error
+   *    HTTP/1.1 500 Internal Server Error
+  */
+  dispatcher.onPost('/login', async function (req, res) {
+    const user = config.users.find(user => {
+      return req.params.username === user.username
+    })
+    let success = false
+    let token = false
+    if (user) {
+      success = await bcrypt.compare(req.params.password, user.password)
+      if (success) {
+        token = jwt.sign({ username: user.username }, config.secret, {
+          expiresIn: 86400
+        })
+      }
+    }
+    res.end(JSON.stringify({
+      success: success,
+      token: token
+    }))
+  })
   /**
    * LIST NODES
    *
    * @api {get} /nodes List all nodes
+   * @apiHeader {String} Authentication User's unique token.
    * @apiGroup Tasks
    * @apiVersion 0.0.1
    * @apiName ListNodes
